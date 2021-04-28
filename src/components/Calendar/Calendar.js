5c3bf973a2c34ebae8886e45098725b2d470ff10
Felix Keller <felixkeller98@gmail.com>
2021-04-28 15:43:55 +0200

    Styling Bubble

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,11 +1,42 @@
 import ButtonAppBar from "../AppBar";
+import usePageData from "../../hooks/usePageData";
+import Bubbles from "../Bubbles";
 
 function Calendar() {
+  const [calendarData, isLoading] = usePageData("Calendar");
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  const calendar = calendarData[0];
+  const todos = calendarData[1];
+
+  console.log("calendar", calendar);
+  console.log("todos", todos);
+
+  console.log("calendarData", calendarData);
   return (
     <>
       <ButtonAppBar title="Calendar" />
+      <Bubbles />
 
       <h1>Calendar</h1>
+      {/* <ul>
+        {calendar.map((data) => (
+          <>
+            <li>Calendar Data: {data.title}</li>
+          </>
+        ))}
+        {todos.map((data) => {
+          console.log("TDO", data);
+          return (
+            <>
+              <li>Todos: {data.title}</li>
+            </>
+          );
+        })}
+      </ul> */}
     </>
   );
 }
--- src/components/Finance/Finance.js
+++ src/components/Finance/Finance.js
@@ -1,11 +1,25 @@
 import ButtonAppBar from "../AppBar";
+import usePageData from "../../hooks/usePageData";
+import Bubbles from "../Bubbles";
 
 function Finance() {
+  const [financeData, isLoading] = usePageData("Finance");
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <ButtonAppBar title="Finanzen" />
+      <Bubbles />
 
       <h1>Finance</h1>
+      <ul>
+        {financeData.map((data) => (
+          <li>Value: {data.value}</li>
+        ))}
+      </ul>
     </>
   );
 }
