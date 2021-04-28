22f23b8bbe94c50c843496500f8fc05d20ac1407
Felix Keller <felixkeller98@gmail.com>
2021-04-28 08:51:33 +0200

    comment todo page out

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import "./App.css";
 import Home from "./components/Home/Home";
-import ToDo from "./components/ToDo/ToDo";
+// import ToDo from "./components/ToDo/ToDo";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import themeLight from "./theme/themeLight";
-import themeDark from "./theme/themeDark";
-import useMediaQueryUI from "@material-ui/core/useMediaQuery";
 
 function App() {
-  const prefersDarkMode = useMediaQueryUI("(prefers-color-scheme: dark)");
   return (
-    <ThemeProvider theme={prefersDarkMode ? themeDark : themeLight}>
+    <ThemeProvider theme={themeLight}>
       <CssBaseline />
       <RecoilRoot>
         <Router>
           <Home path="/" default />
-          <ToDo path="todo" />
+          {/* <ToDo path="todo" /> */}
         </Router>
       </RecoilRoot>
     </ThemeProvider>
