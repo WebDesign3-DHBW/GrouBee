4c8032037c9e0b53242e703649ba69b26bc39a8d
NataschaForster <forsternatascha@gmail.com>
2021-04-28 09:04:55 +0200

    first commit

---
+++ src/components/FAB.js
@@ -0,0 +1,12 @@
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+function FAB() {
+    return <>
+        <Fab color="primary" aria-label="add">
+            <AddIcon />
+        </Fab>
+    </>
+}
+
+export default FAB;
