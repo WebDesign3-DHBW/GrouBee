9659d623acb493de99de150297ab08795ab8f4f7
Felix Keller <felixkeller98@gmail.com>
2021-04-28 09:03:42 +0200

    inital group logic

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,9 +1,23 @@
 import ButtonAppBar from "../AppBar";
+import { Link } from "@reach/router";
+import Bubbles from "../Bubbles";
+import usePageData from "../../hooks/usePageData";
+
 function ToDo() {
+  const [todos, isLoading] = usePageData("ToDo");
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <ButtonAppBar title="ToDo" />
+      <Link to="/">Home</Link>
+      <Bubbles />
       <h1>ToDo</h1>
+      {todos.map((todo, idx) => (
+        <li key={idx}>{todo.title}</li>
+      ))}
     </>
   );
 }
