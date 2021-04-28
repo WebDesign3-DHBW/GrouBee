984bec38ec6df1aa42dfef5eb240e891916a661d
Felix Keller <felixkeller98@gmail.com>
2021-04-28 09:10:34 +0200

    page setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,8 @@ import { Link } from "@reach/router";
 function Home() {
   return (
     <>
-      <Link to="todo">Todo</Link>
+      <Link to="todo">Todo</Link> <Link to="calendar">calendar</Link>{" "}
+      <Link to="finance">finance</Link> <Link to="media">media</Link>
       <h1>Home</h1>
     </>
   );
