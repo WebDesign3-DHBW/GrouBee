3df700ce3f093018ecc7b535a9387ff96bf0a154
OleMantei <hamburgwind@posteo.de>
2021-04-29 15:12:47 +0200

    Update data written to firestore on signup

--- src/auth/signUp.js
+++ src/auth/signUp.js
@@ -7,13 +7,21 @@ export const signUp = async (username, email, password) => {
 
     if (userCreds) {
       const user = userCreds.user;
-      await db.collection("User").doc(user.uid).set(
-        {
-          userId: user.uid,
-          userName: username,
-        },
-        { merge: true }
-      );
+      await db
+        .collection("User")
+        .doc(user.uid)
+        .set(
+          {
+            userId: user.uid,
+            userName: username,
+            profileImage:
+              "https://images.unsplash.com/photo-1600752384899-7d3dcbb2428c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=10",
+            groups: {
+              [user.uid]: "ICH",
+            },
+          },
+          { merge: true }
+        );
       return true;
     }
   } catch (e) {
