ae4207a8ffb3c7ef18509ff3981eb732ec7fc446
Felix Keller <felixkeller98@gmail.com>
2021-04-29 16:03:11 +0200

    add FAB button

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,9 +1,15 @@
 import ButtonAppBar from "../AppBar";
+import Bubbles from "../Bubbles";
+import FAB from "../FAB";
 
 function Settings() {
+  const openModal = () => {};
+
   return (
     <>
       <ButtonAppBar title="Einstellungen" />
+      <Bubbles />
+      <FAB open={openModal} />
       <h1>Settings</h1>
     </>
   );
