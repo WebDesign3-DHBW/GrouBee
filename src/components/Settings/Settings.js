5c624c0e93cf71e51a1673266a61905d9eb35c57
OleMantei <hamburgwind@posteo.de>
2021-04-29 10:12:06 +0200

    Add signout :passport_control:

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,10 +1,23 @@
 import ButtonAppBar from "../AppBar";
+import { Link, navigate } from "@reach/router";
+import Button from "@material-ui/core/Button";
+import { signOut } from "../../auth/signOut";
 
 function Settings() {
+  const onClickSignOut = async () => {
+    try {
+      await signOut();
+      navigate(`/login`);
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
   return (
     <>
       <ButtonAppBar title="Einstellungen" />
       <h1>Settings</h1>
+      <Button onClick={onClickSignOut}>Ausloggen</Button>
     </>
   );
 }
