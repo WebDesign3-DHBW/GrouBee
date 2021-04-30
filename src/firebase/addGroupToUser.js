67d51776698b1c70b61ba70eadb7ef8cd4093872
Felix Keller <felixkeller98@gmail.com>
2021-04-30 11:13:52 +0200

    Get current user id to addGroupToUser-function

--- src/firebase/addGroupToUser.js
+++ src/firebase/addGroupToUser.js
@@ -1,9 +1,9 @@
 import firebase from "firebase/app";
+import { getCurrentUser } from "./getCurrentUser";
 
 export const addGroupToUser = async (groups) => {
-    firebase
-        .firestore()
-        .collection("User").doc("Xq6qvtZxtdrtvpzIfIWP").update({
-            groups: groups
-        });
+  const currentUser = getCurrentUser();
+  firebase.firestore().collection("User").doc(currentUser.id).update({
+    groups: groups,
+  });
 };
