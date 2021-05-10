cbe2b9e43d747c73f56846bef806c97d02aef7d8
NataschaForster <forsternatascha@gmail.com>
2021-05-10 11:33:57 +0200

    fixed warning

--- src/components/ToDo/AddCard.js
+++ src/components/ToDo/AddCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Button,
   Dialog,
