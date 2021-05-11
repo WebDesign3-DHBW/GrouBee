2d6ca0420621ee437c6d7facfec2a4ec18aae3b9
Felix Keller <35599391+felixkeller98@users.noreply.github.com>
2021-05-11 16:30:21 +0200

    Delete getAllTasks

--- src/firebase/getAllTasks.js
+++
@@ -1,6 +0,0 @@
-import firebase from "firebase/app";
-
-export const getAllTasks = async () => {
-  const snapshot = await firebase.firestore().collection("ToDo").get();
-  return snapshot.docs.map((doc) => doc.data());
-};
