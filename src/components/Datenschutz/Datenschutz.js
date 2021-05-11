192e0c3ffd8a1b2be8582b2d3ad4a1753c0d9f54
NataschaForster <forsternatascha@gmail.com>
2021-05-11 16:29:26 +0200

    settings frontend and routes to impressum and datenschutz

---
+++ src/components/Datenschutz/Datenschutz.js
@@ -0,0 +1,13 @@
+import ButtonAppBar from "../AppBar";
+import Wrapper from "../base/Wrapper";
+
+function Datenschutz() {
+  return (
+    <>
+      <ButtonAppBar title="Datenschutz" />
+      <Wrapper></Wrapper>
+    </>
+  );
+}
+
+export default Datenschutz;
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -3,15 +3,25 @@ import FAB from "../FAB";
 import ButtonAppBar from "../AppBar";
 import GroupPopup from "./GroupPopup";
 import { useState } from "react";
-import { Button } from "@material-ui/core";
+import { Button, Divider, makeStyles } from "@material-ui/core";
 import ProfilePopup from "./ProfilePopup";
 import GroupLink from "./GroupLink";
+import Wrapper from "../base/Wrapper";
+import { navigate } from "@reach/router";
+const useStyles = makeStyles((theme) => ({
+  button: {
+    padding: 0,
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+}));
 
 function Settings() {
   const [openGroupPopup, setOpenGroupPopup] = useState(false);
   const [openEditProfile, setOpenEditProfile] = useState(false);
   const [tiggerUpdate, setTriggerUpdate] = useState(false);
   const [openGroupLink, setOpenGroupLink] = useState(false);
+  const classes = useStyles();
 
   return (
     <>
@@ -24,10 +34,40 @@ function Settings() {
         close={() => setOpenGroupPopup(false)}
         updateBubbles={() => setTriggerUpdate(!tiggerUpdate)}
       />
-      <Button onClick={() => setOpenEditProfile(true)}>Profil bearbeiten</Button>
-      <ProfilePopup open={openEditProfile} close={() => setOpenEditProfile(false)} />
+      <Wrapper>
+        <FAB open={() => setOpenGroupPopup(true)} />
+        <Divider />
+        <Button className={classes.button} onClick={() => setOpenEditProfile(true)}>
+          Profil bearbeiten
+        </Button>
+        <Divider />
+        <Button
+          className={classes.button}
+          onClick={() => {
+            navigate("/impressum");
+          }}
+        >
+          Impressum
+        </Button>
+        <Divider />
+        <Button
+          className={classes.button}
+          onClick={() => {
+            navigate("/datenschutz");
+          }}
+        >
+          Datenschutz
+        </Button>
+        <Divider />
+        <Popup
+          open={openGroupPopup}
+          close={() => setOpenGroupPopup(false)}
+          updateBubbles={() => setTriggerUpdate(!tiggerUpdate)}
+        />
+        <ProfilePopup open={openEditProfile} close={() => setOpenEditProfile(false)} />
 
-      <GroupLink open={openGroupLink} close={() => setOpenGroupLink(false)} />
+        <GroupLink open={openGroupLink} close={() => setOpenGroupLink(false)} />
+      </Wrapper>
     </>
   );
 }
