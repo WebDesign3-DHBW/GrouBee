f40cd7e5282d246f7850aeac10207969800b9ee8
NataschaForster <forsternatascha@gmail.com>
2021-05-12 08:29:38 +0200

    change buttons to list

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -3,7 +3,7 @@ import FAB from "../FAB";
 import ButtonAppBar from "../AppBar";
 import Popup from "../Settings/Popup";
 import { useState } from "react";
-import { Button, Divider, makeStyles } from "@material-ui/core";
+import { Divider, List, ListItem, ListItemText, makeStyles } from "@material-ui/core";
 import ProfilePopup from "./ProfilePopup";
 import GroupLink from "./GroupLink";
 import Wrapper from "../base/Wrapper";
@@ -11,8 +11,8 @@ import { navigate } from "@reach/router";
 const useStyles = makeStyles((theme) => ({
   button: {
     padding: 0,
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
   },
 }));
 
@@ -28,37 +28,40 @@ function Settings() {
       <ButtonAppBar title="Einstellungen" />
       <Bubbles updateMe={tiggerUpdate} />
       <Wrapper>
+        <List component="nav" aria-label="main mailbox folders">
+          <Divider />
+          <ListItem button className={classes.button} onClick={() => setOpenEditProfile(true)}>
+            <ListItemText primary="Profil bearbeiten" />
+          </ListItem>
+          <Divider />
+          <ListItem
+            button
+            className={classes.button}
+            onClick={() => {
+              navigate("/impressum");
+            }}
+          >
+            <ListItemText primary="Impressum" />
+          </ListItem>
+          <Divider />
+          <ListItem
+            button
+            className={classes.button}
+            onClick={() => {
+              navigate("/datenschutz");
+            }}
+          >
+            <ListItemText primary="Datenschutz" />
+          </ListItem>
+          <Divider />
+        </List>
         <FAB open={() => setOpenGroupPopup(true)} />
-        <Divider />
-        <Button className={classes.button} onClick={() => setOpenEditProfile(true)}>
-          Profil bearbeiten
-        </Button>
-        <Divider />
-        <Button
-          className={classes.button}
-          onClick={() => {
-            navigate("/impressum");
-          }}
-        >
-          Impressum
-        </Button>
-        <Divider />
-        <Button
-          className={classes.button}
-          onClick={() => {
-            navigate("/datenschutz");
-          }}
-        >
-          Datenschutz
-        </Button>
-        <Divider />
         <Popup
           open={openGroupPopup}
           close={() => setOpenGroupPopup(false)}
           updateBubbles={() => setTriggerUpdate(!tiggerUpdate)}
         />
         <ProfilePopup open={openEditProfile} close={() => setOpenEditProfile(false)} />
-
         <GroupLink open={openGroupLink} close={() => setOpenGroupLink(false)} />
       </Wrapper>
     </>
