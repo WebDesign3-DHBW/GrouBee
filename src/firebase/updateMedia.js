a18cff84cb9ad083133548276d05514eb7ec6648
Felix Keller <felixkeller98@gmail.com>
2021-05-12 09:11:31 +0200

    fix warning

--- src/firebase/updateMedia.js
+++ src/firebase/updateMedia.js
@@ -3,7 +3,7 @@ import firebase from "firebase/app";
 export const updateMedia = async (docID, update) => {
   try {
     const mediaRef = firebase.firestore().collection("Media").doc(docID);
-    const res = await mediaRef.update(update);
+    await mediaRef.update(update);
   } catch (e) {
     console.error("Upsi", e);
   }
