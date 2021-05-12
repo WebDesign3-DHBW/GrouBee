4b5b391b422ce3a3de07c38d1ac7a22901b850dd
OleMantei <hamburgwind@posteo.de>
2021-05-12 10:20:16 +0200

    add animation to bubbles

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -3,6 +3,7 @@ import { activeGroupsState } from "../utils/recoil";
 import { useRecoilState } from "recoil";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { colors } from "../theme/bubbleColors";
+import { motion } from "framer-motion";
 
 import Bubble from "./Bubble";
 import { useCurrentUser } from "../hooks/useCurrentUser";
@@ -41,32 +42,40 @@ function Bubbles({ updateMe }) {
 
   if (isLoading) {
     return (
-      <div style={{ display: "flex" }}>
-        <Skeleton variant="circle" width={64} height={64} className={classes.skeleton} />
-        <Skeleton variant="circle" width={64} height={64} className={classes.skeleton} />
-        <Skeleton variant="circle" width={64} height={64} className={classes.skeleton} />
-      </div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: [0, 1] }} transition={{ delay: 1 }}>
+        <div style={{ display: "flex" }}>
+          <Skeleton variant="circle" width={64} height={64} className={classes.skeleton} />
+          <Skeleton variant="circle" width={64} height={64} className={classes.skeleton} />
+          <Skeleton variant="circle" width={64} height={64} className={classes.skeleton} />
+        </div>
+      </motion.div>
     );
   }
 
   return (
     <div className={classes.root}>
-      {Object.entries(userData.groups)
-        // Sort by group name
-        .sort((a, b) => a[1].localeCompare(b[1]))
-        // Place "ICH" at the beginning
-        .sort((a, b) => (b[1] === "ICH") - (a[1] === "ICH"))
-        .map((group, idx) => {
-          return (
-            <Bubble
-              key={idx}
-              group={group}
-              toggleElement={toggleElement}
-              activeGroups={activeGroups}
-              color={colors[idx]}
-            />
-          );
-        })}
+      <motion.div
+        initial={{ opacity: 0, x: 250 }}
+        animate={{ opacity: [0, 1], x: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        {Object.entries(userData.groups)
+          // Sort by group name
+          .sort((a, b) => a[1].localeCompare(b[1]))
+          // Place "ICH" at the beginning
+          .sort((a, b) => (b[1] === "ICH") - (a[1] === "ICH"))
+          .map((group, idx) => {
+            return (
+              <Bubble
+                key={idx}
+                group={group}
+                toggleElement={toggleElement}
+                activeGroups={activeGroups}
+                color={colors[idx]}
+              />
+            );
+          })}
+      </motion.div>
     </div>
   );
 }
