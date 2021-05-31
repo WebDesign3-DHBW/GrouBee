bf7ccfdedc573132475e406021c315d84c44bbf5
OleMantei <hamburgwind@posteo.de>
2021-05-31 10:45:14 +0200

    improve finance site and import needed components

--- src/components/Finance/Finance.js
+++ src/components/Finance/Finance.js
@@ -1,8 +1,16 @@
+//import { makeStyles } from "@material-ui/core/styles";
+import Wrapper from "../base/Wrapper";
 import ButtonAppBar from "../AppBar";
 import usePageData from "../../hooks/usePageData";
 import Bubbles from "../Bubbles";
+import { Divider } from "@material-ui/core";
+import BalanceCard from "./BalanceCard";
+import ExpenseItem from "./ExpenseItem";
+
+//const useStyles = makeStyles((theme) => ({}));
 
 function Finance() {
+  //const classes = useStyles();
   const [financeData, isLoading] = usePageData("Finance");
 
   if (isLoading) {
@@ -14,13 +22,23 @@ function Finance() {
     <>
       <ButtonAppBar title="Finanzen" />
       <Bubbles />
-
-      <h1>Finance</h1>
-      <ul>
+      <Wrapper>
+        <BalanceCard value="9,80" />
+        <BalanceCard value="15,20" group="WG" />
+        <BalanceCard value="-5,40" group="Friends" />
         {financeData.map((data) => (
-          <li>Value: {data.value}</li>
+          <>
+            <ExpenseItem
+              title="Snacks"
+              name="Hannes"
+              date="Mi., 28. April 2021"
+              value={data.value}
+              settled={false}
+            />
+            <Divider />
+          </>
         ))}
-      </ul>
+      </Wrapper>
     </>
   );
 }
