3ee39a3bbf7d938133d2553115fcb38491df76db
OleMantei <hamburgwind@posteo.de>
2021-06-02 13:16:38 +0200

    add files from 27-frontend-finances

---
+++ src/components/Finance/ExpenseItem.js
@@ -0,0 +1,132 @@
+import {
+  Avatar,
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { getUserData } from "../../firebase/getUserData";
+import ExpenseItemSkeleton from "./ExpenseItemSkeleton";
+
+const useStyles = makeStyles((theme) => ({
+  crossedOut: {
+    textDecoration: "line-through",
+  },
+  textPrimary: {
+    color: theme.palette.primary.main,
+  },
+}));
+
+function ExpenseItem(props) {
+  const classes = useStyles();
+  const [dataLoading, setDataLoading] = useState(true);
+  const [profileImage, setProfileImage] = useState();
+  const [profileName, setProfileName] = useState();
+  const [groupName, setGroupName] = useState();
+  const [settled, setSettled] = useState(false);
+  const [nextSettled, setNextSettled] = useState(false);
+  const [settledDate, setSettledDate] = useState();
+
+  useEffect(() => {
+    const loadUserData = async () => {
+      const userData = await getUserData(props.paidBy);
+      try {
+        setProfileImage(userData.profileImage);
+        setProfileName(userData.userName);
+        setGroupName(props.currentUserData.groups[props.groupID]);
+      } catch {}
+    };
+    loadUserData();
+    function checkSettled() {
+      if (Object(props.settlementData).length !== 0 && props.settlementData !== null) {
+        props.settlementData.map((data, i) => {
+          if (props.groupID === data.groupID) {
+            setSettled(new Date(data.settleDate) > new Date(props.currentDate));
+          }
+          return null;
+        });
+      }
+    }
+    checkSettled();
+    function getNext() {
+      const sortedDataLength = Object(props.sortedData).length;
+      let nextKey = 1;
+      try {
+        while (sortedDataLength - props.ID !== nextKey) {
+          if (props.ID !== sortedDataLength) {
+            let nextGroupID = props.sortedData[props.ID + nextKey].groupID;
+            const nextDate = props.sortedData[props.ID + nextKey].currentDate;
+            if (props.groupID !== nextGroupID) {
+              nextKey = nextKey + 1;
+            }
+            if (props.groupID === nextGroupID) {
+              nextKey = sortedDataLength + 1;
+              props.settlementData.map((data, i) => {
+                setNextSettled(new Date(data.settleDate) > new Date(nextDate));
+                new Date(data.settleDate) > new Date(nextDate) && setSettledDate(data.settleDate);
+                return null;
+              });
+            }
+          }
+        }
+      } catch {}
+    }
+    getNext();
+    setDataLoading(false);
+  }, [props]);
+
+  if (dataLoading) {
+    return <ExpenseItemSkeleton />;
+  } else {
+    return (
+      <>
+        <ListItem className="nplr">
+          <ListItemAvatar>
+            <Avatar alt="Avatar" src={profileImage} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={props.title}
+            secondary={
+              groupName &&
+              `${profileName} ${props.multipleSelected ? "@" + groupName + " | " : "|"} ${new Date(
+                props.currentDate
+              ).toLocaleDateString("de-DE", {
+                weekday: "short",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}`
+            }
+          />
+          <ListItemSecondaryAction className="nr">
+            <Typography
+              className={`${settled && classes.crossedOut} ${!settled && classes.textPrimary}`}
+            >
+              {props.expense} €
+            </Typography>
+          </ListItemSecondaryAction>
+        </ListItem>
+        {!settled && nextSettled && (
+          <>
+            <Divider />
+            <Typography variant="overline" color="primary">
+              {groupName} beglichen{" "}
+              {new Date(settledDate).toLocaleDateString("de-DE", {
+                weekday: "short",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </Typography>
+          </>
+        )}
+      </>
+    );
+  }
+}
+
+export default ExpenseItem;
--- src/components/Finance/Finance.js
+++ src/components/Finance/Finance.js
@@ -1,18 +1,86 @@
+import { getCurrentUserData } from "../../firebase/getCurrentUserData";
+import { getSettlementData } from "../../firebase/getSettlementData";
+import Wrapper from "../base/Wrapper";
 import ButtonAppBar from "../AppBar";
 import usePageData from "../../hooks/usePageData";
 import Bubbles from "../Bubbles";
 import FAB from "../FAB";
 import FinancePopup from "./FinancePopup";
-import { useState } from "react";
+import { useEffect, seState } from "react";
+import { Divider, makeStyles, Typography } from "@material-ui/core";
+import ExpenseItem from "./ExpenseItem";
+import { useRecoilValue } from "recoil";
+import { activeGroupsState } from "../../utils/recoil";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import ExpenseItemSkeleton from "./ExpenseItemSkeleton";
+
+const useStyles = makeStyles((theme) => ({
+  center: {
+    textAlign: "center",
+  },
+  info: {
+    display: "flex",
+    marginTop: "20vh",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  infoIcon: {
+    fontSize: "1.5rem",
+    marginRight: theme.spacing(1),
+  },
+}));
 
 function Finance() {
+  const classes = useStyles();
+  const [dataLoading, setDataLoading] = useState(true);
+  const [currentUserData, setCurrentUserData] = useState();
   const [financeData, isLoading] = usePageData("Finance");
   const [openFinancePopup, setOpenFinancePopup] = useState(false);
+  const [multipleSelected, setMultipleSelected] = useState(false);
+  const [sortedData, setSortedData] = useState(null);
+  const [settlementData, setSettlementData] = useState(null);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  const activeGroups = useRecoilValue(activeGroupsState);
+
+  useEffect(() => {
+    const activeGroupIDs = activeGroups.map((groupArr) => groupArr[0]);
+    const loadUserData = async () => {
+      const userData = await getCurrentUserData();
+      setCurrentUserData(userData);
+    };
+    loadUserData();
+    function multipleGroupsSelected() {
+      1 < Object.keys(activeGroupIDs).length
+        ? setMultipleSelected(true)
+        : setMultipleSelected(false);
+    }
+    multipleGroupsSelected();
+    financeData &&
+      setSortedData(
+        financeData.sort(function (a, b) {
+          var dateA = new Date(a.currentDate),
+            dateB = new Date(b.currentDate);
+          return dateB - dateA;
+        })
+      );
+    const handleSettlementData = async () => {
+      if (Object.keys(activeGroupIDs).length !== 0) {
+        const settlementData = await getSettlementData(activeGroupIDs);
+        setSettlementData(settlementData);
+      }
+    };
+    handleSettlementData();
+    setDataLoading(false);
+  }, [financeData, activeGroups]);
+
+  function loadingSkeleton() {
+    const n = 6;
+    return [...Array(n)].map((e, i) => (
+      <div key={i}>
+        <ExpenseItemSkeleton />
+      </div>
+    ));
   }
-  console.log("financeData", financeData);
 
   return (
     <>
@@ -20,13 +88,39 @@ function Finance() {
       <Bubbles />
       <FAB open={() => setOpenFinancePopup(true)} />
       <FinancePopup open={openFinancePopup} close={() => setOpenFinancePopup(false)} />
-
-      <h1>Finance</h1>
-      <ul>
-        {financeData.map((data) => (
-          <li>Value: {data.value}</li>
-        ))}
-      </ul>
+      <Wrapper>
+        {dataLoading && isLoading && activeGroups.length !== 0
+          ? loadingSkeleton()
+          : Object(sortedData).length !== 0 &&
+            sortedData !== null &&
+            sortedData.map((data, i) => (
+              <div key={i}>
+                <ExpenseItem
+                  currentUserData={currentUserData}
+                  title={data.title}
+                  currentDate={data.currentDate}
+                  expense={data.expense}
+                  paidBy={data.paidBy}
+                  groupID={data.groupID}
+                  multipleSelected={multipleSelected}
+                  settlementData={settlementData}
+                  sortedData={sortedData}
+                  ID={i}
+                />
+                <Divider />
+              </div>
+            ))}
+        {activeGroups.length === 0 && (
+          <Typography
+            variant="subtitle1"
+            color="textSecondary"
+            className={`${classes.info} ${classes.center}`}
+          >
+            <AiOutlineInfoCircle className={classes.infoIcon} />
+            Gruppe/-n auswählen
+          </Typography>
+        )}
+      </Wrapper>
     </>
   );
 }
