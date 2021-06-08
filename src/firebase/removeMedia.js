7a58bf7398e6995d0896cc6388fc9fdd41eb91b0
Felix Keller <felixkeller98@gmail.com>
2021-06-08 10:04:03 +0200

    remove unnecessary firebase functions

--- src/firebase/removeMedia.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase/app";
-
-export const removeMedia = async (docID) => {
-  try {
-    await firebase.firestore().collection("Media").doc(docID).delete();
-  } catch (e) {
-    console.error("Upsi", e);
-  }
-};
